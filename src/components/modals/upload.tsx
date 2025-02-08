b9d738b52bb6cdb9a4ff240ad5ede3fba3b1887e
Your Name <you@example.com>
2025-02-08 19:04:22 +0100

    fixing loading state for upload btn and cleanup

--- src/components/modals/upload.tsx
+++ src/components/modals/upload.tsx
@@ -1,32 +1,28 @@
-
 'use client'
-
-import { useChat } from 'ai/react';
 import { useState } from 'react';
+import Button from '../button';
 interface CVUploaderProps {
   closeModal: () => void;
-  setInput: (input: string) => void;}
+  setInput: (input: string) => void;
+}
 
 const CVUploader = ({ closeModal, setInput }: CVUploaderProps) => {
-  // const { setInput, input } = useChat({
-  //   api: "/api/processed",
-  // });
   const [file, setFile] = useState<File | null>(null);
   const [summary, setSummary] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleUpload = async () => {
     if (!file) return alert('Please select a file');
+
+    setIsLoading(true);
 
     const formData = new FormData();
     formData.append('file', file);
 
     const res = await fetch('/api/uploads', { method: 'POST', body: formData });
     const data = await res.json();
-    console.log({ data });
 
     if (data.filePath) {
-      console.log('clicked');
-
       const processRes = await fetch('/api/processed', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -35,26 +31,28 @@ const CVUploader = ({ closeModal, setInput }: CVUploaderProps) => {
 
       const processData = await processRes.json();
       const extracted = processData.extractedText;
-      console.log("processdata", processData);
       setInput(extracted);
       setSummary(extracted);
     }
-    // console.log({input});
-    
 
+    setIsLoading(false);
     closeModal();
   };
 
   return (
     <div className="p-6 border rounded-md w-96 bg-white shadow-md">
       <input type="file" accept=".pdf,.docx" onChange={(e) => setFile(e.target.files?.[0] || null)} />
-      <button onClick={handleUpload} className="mt-4 p-2 bg-blue-500 text-white rounded">
-        Upload & Process
-      </button>
-      {summary && <p className="mt-4 p-2 border rounded">{summary}</p>}
+      <Button
+        onClick={handleUpload}
+        className="mt-4 p-2 bg-blue-500 text-white rounded"
+        disabled={isLoading}
+        isLoading={isLoading}
+      
+      >
+       Upload & Process
+      </Button>
     </div>
   );
 };
 
 export default CVUploader;
-
