19bc61dc880e83d782b8c13a90f9b6621fc688c0
Your Name <you@example.com>
2025-02-16 20:49:42 +0100

    adding comming soon component for route not yet build, will tryx to see how to manage loadx states.

--- src/components/coming-soon.tsx
+++ src/components/coming-soon.tsx
@@ -23,7 +23,6 @@ const ComingSoon = () => {
       <div className="mt-8 flex space-x-4">
         <Link href="#" className="text-white hover:text-indigo-300">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            {/* ... */}
           </svg>
         </Link>
       </div>
